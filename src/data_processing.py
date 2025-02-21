d421f56ba2be2463e98c9b09466203b2ef5bf96b
Dmitry Morozov <morozov124@gmail.com>
2025-02-21 22:28:57 +0300

    The function has been completely rewritten.

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,60 +1,59 @@
+from datetime import timedelta
 from decimal import Decimal
 from typing import cast
 
 from project_settings import setting
 from src.db.crud import db_read, db_update
-from src.db.queries import data_for_calculation, get_next_record, insert_processed_data
-from src.db.schemas import Record, NewRecord
+from src.db.queries import data_for_calculation, insert_processed_data
+from src.db.schemas import Record
 
 
 async def data_processing(ticker: str) -> None:
     """processing of data according to specified settings"""
 
-    # record: Record
-    # next_record: NewRecord
-    next_record_lst: list[NewRecord] = []
-
     data = cast(list[Record], await db_read(
         data_for_calculation, {
             'ticker': ticker,
             'dividends_purchase_day_offset': setting.DIVIDENDS_PURCHASE_DAY_OFFSET,
-            'monthly_purchase_day': setting.MONTHLY_PURCHASE_DAY}
+        }
     ))  # to satisfy mypy
 
     shares = monthly_balance = expenses = Decimal(0)
+    day = setting.MONTHLY_PURCHASE_DAY
+    dividends = None
+    sentinel = setting.LIMIT_OF_DAYS_FOR_PRICE_SEARCH
 
     for record in data:
+        dividends = record.value if not dividends else dividends
         price = record.closing_price
         date = record.trade_date
-        if not record.closing_price:
-            for _ in range(setting.LIMIT_OF_DAYS_FOR_PRICE_SEARCH):
-                next_record_lst = cast(
-                    list[NewRecord], await db_read(get_next_record, {'ticker': ticker, 'date': date})
-                )  # to satisfy mypy
-                if next_record_lst:
-                    next_record, *_ = next_record_lst
-                    if next_record.closing_price:
-                        price = next_record.closing_price
-                        date = next_record.trade_date
-                        break
-                    else:
-                        date = next_record.trade_date
-                        continue
-                else:
-                    break
-            # end of records reached
-            if not next_record_lst:
-                break
 
-        if not record.value:
-            expenses += setting.MONTHLY_INVESTMENTS
-            shares = shares + (setting.MONTHLY_INVESTMENTS + monthly_balance) // price
-            monthly_balance = (setting.MONTHLY_INVESTMENTS + monthly_balance) % price
-            capitalization = shares * price
-        else:
-            shares = shares + (record.value * shares + monthly_balance) // price
-            monthly_balance = (record.value * shares + monthly_balance) % price
-            capitalization = shares * price
+        if dividends:
+            if price:
+                income = dividends * shares
+                shares = shares + (income + monthly_balance) // price
+                monthly_balance = (income + monthly_balance) % price
+
+                dividends = None
+
+        if day == date.day:
+            if price:
+                expenses += setting.MONTHLY_INVESTMENTS
+                shares = shares + (setting.MONTHLY_INVESTMENTS + monthly_balance) // price
+                monthly_balance = (setting.MONTHLY_INVESTMENTS + monthly_balance) % price
+
+                day = setting.MONTHLY_PURCHASE_DAY
+                sentinel = setting.LIMIT_OF_DAYS_FOR_PRICE_SEARCH
+            else:
+                day = (date + timedelta(days=1)).day
+                sentinel -= 1
+                if not sentinel:
+                    break
+
+        if not price:
+            continue
+
+        capitalization = shares * price
 
         new_record = date, ticker, expenses, shares, capitalization, price, monthly_balance
         await db_update(insert_processed_data, (new_record,))
