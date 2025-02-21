eafa9ba25cc4a69e237a123acd20a3ec8bd337f7
Dmitry Morozov <morozov124@gmail.com>
2025-02-21 22:41:25 +0300

    Fixed fixtures.

--- tests/crud/conftest.py
+++ tests/crud/conftest.py
@@ -1,4 +1,4 @@
-import datetime
+import os
 import subprocess
 from time import sleep
 from typing import Iterator, AsyncIterator
@@ -9,34 +9,23 @@ from psycopg import AsyncConnection
 
 from src.db.connection import connection
 
-query = """                                                                                                            
-        CREATE TABLE test_table(                                                                                       
-        id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,                                                               
-        date date,                                                                                                     
-        ticker varchar                                                                                                 
-        );                                                                                                             
-        """
-insert_data = """                                                                                                      
-        INSERT INTO test_table(                                                                                        
-        date,                                                                                                          
-        ticker                                                                                                         
-        ) VALUES (%s,%s);                                                                                              
-        """
-data = ('2020-1-1', 'SBER')
-read = """                                                                                                             
-        SELECT * FROM test_table                                                                                       
-        """
-data_result = [(1, datetime.date(2020, 1, 1), 'SBER')]
 
-
-@pytest.fixture(scope='module', autouse=True)
+@pytest.fixture(scope='module')
 def start_db() -> Iterator[None]:
-    subprocess.run('docker compose -f docker_for_tests.yml up -d', shell=True, capture_output=True)
+    subprocess.run(
+        'docker compose -f docker_for_tests.yml up -d',
+        shell=True,
+        capture_output=True
+    )
     sleep(5)
     try:
         yield
     finally:
-        subprocess.run('docker compose -f docker_for_tests.yml down', shell=True, capture_output=True)
+        subprocess.run(
+            'docker compose -f docker_for_tests.yml down',
+            shell=True,
+            capture_output=True
+        )
 
 
 @pytest.fixture(scope='function')
@@ -44,14 +33,15 @@ async def set_connection() -> AsyncIterator[AsyncConnection]:
     logger.remove()
     pool = await anext(connection)
     async with pool.connection() as aconn:
+        await aconn.set_autocommit(True)
         yield aconn
 
 
-@pytest.fixture(scope='function', autouse=True)
+@pytest.fixture(scope='function')
 async def clean_after_test() -> AsyncIterator[None]:
     yield
     pool = await anext(connection)
     async with pool.connection() as aconn:
         await aconn.execute(
-            """DROP TABLE IF EXISTS test_table;"""
+            """DROP TABLE IF EXISTS test_table, results_of_trades, dividends, processed_data;"""
         )
