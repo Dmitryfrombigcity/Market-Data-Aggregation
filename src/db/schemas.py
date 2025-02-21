4350f1d95ee197e2cccb354e3519cb8367dfc2b1
Dmitry Morozov <morozov124@gmail.com>
2025-02-21 22:31:35 +0300

    class New Record is no longer needed.

--- src/db/schemas.py
+++ src/db/schemas.py
@@ -3,13 +3,10 @@ from decimal import Decimal
 from typing import NamedTuple
 
 
-class NewRecord(NamedTuple):
+class Record(NamedTuple):
     trade_date: datetime.date
     ticker: str
     closing_price: Decimal
-
-
-class Record(NewRecord):
     value: Decimal
 
 
