b0bd14c9e0038235d907c174a25725ebee0bf46e
Dmitry Morozov <morozov124@gmail.com>
2025-04-09 16:26:40 +0300

    Added `path`.

--- tests/crud/conftest.py
+++ tests/crud/conftest.py
@@ -1,5 +1,5 @@
 import subprocess
-import sys
+from pathlib import Path
 from time import sleep
 from typing import Iterator, AsyncIterator
 
@@ -12,11 +12,7 @@ from src.db.connection import connection
 
 @pytest.fixture(scope='module')
 def start_db() -> Iterator[None]:
-
-    path = 'tests/docker_for_tests.yml'
-    if sys.platform == "win32":
-        path = 'tests\\docker_for_tests.yml'
-
+    path = Path(Path(__file__).resolve().parent.parent / 'docker_for_tests.yml')
     subprocess.run(
         f'docker compose -f{path} up -d',
         shell=True,
